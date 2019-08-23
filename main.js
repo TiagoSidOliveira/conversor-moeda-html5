a74a9ed4c51cb77a0072c7b5821edea338459429
Tiago Henrique Silva <tiago.silva@tekno.com.br>
2019-08-23 22:43:05 +0000

    parametros

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 function chamarAPI(){
 	console.log("Chamando API");
 	const endpoint = "https://api.exchangeratesapi.io/latest";
-	$.get(endpoint, callback);
-	
+	var oParametros = {
+		base: $("#moeda_origem").val()
+	};
+	$.get(endpoint, oParametros, callback );
 }
 
 function callback(resultado){
-	console.log(resultado);
+	//console.log(resultado);
 	
 	var sMoedaDestino = $("#moeda_destino").val();
 	var fvalor= resultado.rates[sMoedaDestino];
